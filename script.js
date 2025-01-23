f0d422ed3c77bf8d4f6cc3388731b16c19b22c53
NeonKaiser <128339548+NeonArceuthobium@users.noreply.github.com>
2025-01-23 19:50:50 +0000

    Update index.html, script.js, and style.css for confetti feature

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("website is working");
+
+// Source for library
+// https://confettijs.org/confetti.min.js
+
+// Create variables, instantiate `new` instance of `Confetti`
+let confetti = new Confetti("confetti");
+
+console.log(confetti);
+
+confetti.destroyTarget(false);
+confetti.setSize(10);
+confetti.setPower(20);
+confetti.setFade(true);
+confetti.setCount(100);
